f3d31389146120bdb5fbace5d27a698c122add07
hsheikh7 <49715207+hsheikh7@users.noreply.github.com>
2023-05-06 07:07:08 +0000

    Created Blog's model

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model): 
+    #This is a class for blog app 
+    image = models.ImageField(null= True , blank= True)
+    title = models.CharField(max_length = 255)
+    content = models.TextField()
+    author = models.ForeginKey(User, on_delete = models.CASCADE)
+    status = models.BooleanField()
+    category = models.ForeignKey('Category' , on_delete= models.SET_NULL, null = True )
+
+    created_date = models.DateTimeField(auto_now_add = True)
+    updated_date =  models.DateTimeField(auto_now = True)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title 
+
+class Category(models.Model): 
+    name = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.name 
