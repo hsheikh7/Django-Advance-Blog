ba1667cb7c1c87eb769823ea1be3a46fbcc763ad
hsheikh7 <49715207+hsheikh7@users.noreply.github.com>
2023-05-06 16:45:33 +0000

    Arranged configuration of accounts' admin.py

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User 
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = = ("email", "is_superuser", "is_active", "is_verified")
+    list_filter = = ("email", "is_superuser", "is_active", "is_verified")
+    searching_fields = ("email",)
+    ordering = ("email",)
+
+admin.site.register(User, CustomUserAdmin)
